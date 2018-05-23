6e1c2733413ac5706eb6ab26e12fb7e7712fc8f3
澜嵚 <zhenjiang.szj@alibaba-inc.com>
2018-05-23 19:58:25 +0800

    performance measurement

--- src/index.js
+++ src/index.js
@@ -1,16 +1,55 @@
 'use strict';
 
-!(function() {
-  if ('PerformanceLongTaskTiming' in window) {
-    var g = (window.__tti = { e: [] });
-    g.o = new PerformanceObserver(function(l) {
-      g.e = g.e.concat(l.getEntries());
-    });
-    g.o.observe({ entryTypes: ['longtask'] });
-  }
-})();
+import ttiPolyfill from './path/to/tti-polyfill.js';
 
-// 性能度量结果对象数组
+// 利用 requestIdleCallback 调度任务执行
+requestIdleCallback(myNonEssentialWork, { timeout: 2000 });
+
+function myNonEssentialWork(deadline) {
+  while ((deadline.timeRemaining() > 0 || deadline.didTimeout) && tasks.length > 0) {
+    doWorkIfNeeded();
+  }
+  if (tasks.length > 0) {
+    requestIdleCallback(myNonEssentialWork);
+  }
+}
+
+// 测量 tti
+ttiPolyfill.getFirstConsistentlyInteractive().then((tti) => {
+  ga('send', 'event', {
+    eventCategory: 'Performance Metrics',
+    eventAction: 'TTI',
+    eventValue: tti,
+    nonInteraction: true,
+  });
+});
+
+// 测量自定义时间标记，如 FMP
+function measurePerf() {
+  var perfEntries = performance.getEntriesByType("mark");
+  for (var i = 0; i < perfEntries.length; i++) {
+    console.log("Name: " + perfEntries[i].name +
+      " Entry Type: " + perfEntries[i].entryType +
+      " Start Time: " + perfEntries[i].startTime +
+      " Duration: "   + perfEntries[i].duration  + "\n");
+  }
+}
+
+// 测量长任务
+const observer = new PerformanceObserver((list) => {
+  for (const entry of list.getEntries()) {
+    ga('send', 'event', {
+      eventCategory: 'Performance Metrics',
+      eventAction: 'longtask',
+      eventValue: Math.round(entry.startTime + entry.duration),
+      eventLabel: JSON.stringify(entry.attribution),
+    });
+  }
+});
+
+observer.observe({entryTypes: ['longtask']});
+
+// 测量 FP/FCP
 const metrics = [];
 
 if ('PerformanceLongTaskTiming' in window) {
